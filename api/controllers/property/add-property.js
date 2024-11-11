eb5d0e549765aa8f8937c6206f3e9e1f3d5648b5
Muhammad Attique Anwar <muhammad.attique@falconitracking.com>
2024-11-11 05:35:22 +0500

    Now property image can be added to s3

--- api/controllers/property/add-property.js
+++ api/controllers/property/add-property.js
@@ -1,5 +1,5 @@
 // controller for adding a new property
-
+const { uploadtos3 } = require('../image/uploadtos3');
 module.exports = {
   friendlyName: "Add property",
   description: "",
@@ -58,42 +58,34 @@ module.exports = {
   },
 
   fn: async function (inputs, exits) {
-    let { req } = this;
-    let { property_name,
-      property_price,
-      property_address,
-      property_baths,
-      property_beds,
-      property_area,
-      property_owner,
-      property_image,
-      property_description } = inputs;
-
-
     try {
-      let property = await Property.create({
-        property_name,
-        property_price,
-        property_address,
-        property_baths,
-        property_beds,
-        property_area,
-        property_owner,
-        property_image,
-        property_description
-      });
+        // Assuming `property_image` is a base64 string (remove data URI prefix for AWS S3)
+        const base64Data = Buffer.from(inputs.property_image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+        const imageUrl = await uploadtos3(base64Data, `${Date.now()}-${inputs.property_name}.jpg`, 'image/jpeg');
 
-      return exits.success({
-        message: "Property added successfully",
-        data: property,
-      });
+        // Save image URL in the database
+        let property = await Property.create({
+            property_name: inputs.property_name,
+            property_price: inputs.property_price,
+            property_address: inputs.property_address,
+            property_baths: inputs.property_baths,
+            property_beds: inputs.property_beds,
+            property_area: inputs.property_area,
+            property_owner: inputs.property_owner,
+            property_image: imageUrl,  // Store URL instead of data URL
+            property_description: inputs.property_description,
+        });
 
+        return exits.success({
+            message: "Property added successfully",
+            data: property,
+        });
     } catch (err) {
-      console.log({ err });
-      return exits.exception({
-        message: err.message,
-        err,
-      });
+        console.log({ err });
+        return exits.exception({
+            message: err.message,
+            err,
+        });
     }
-  },
+},
 };
